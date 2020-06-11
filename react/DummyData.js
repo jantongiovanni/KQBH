b6c8ed28c783a1897ed70995f1e13d9ec1ae2f31
Joe Anton <janton153@gmail.com>
2020-06-11 16:05:15 -0700

    fixes to dummy data

---
+++ react/DummyData.js
@@ -0,0 +1,22 @@
+export const dummydata = [
+    {
+      id: 1,
+      title: 'Now Playing',
+      image: require('../images/cat-spinning-records.jpg')
+    },
+    {
+      id: 2,
+      title: 'Schedule',
+      image: require('../images/sountracks-from-the-streets.png')
+    },
+    {
+      id: 3,
+      title: 'News',
+      image: require('../images/testday.jpg')
+    },
+    {
+      id: 4,
+      title: 'Archives',
+      image: require('../images/force.jpeg')
+    },
+];
