44e79569e573dfcb7daf8d6ed87647643cf381a6
Joe Anton <janton153@gmail.com>
2020-06-12 22:59:35 -0700

    Feather Icons | Fade In back button

--- react/screens/DetailScreen.js
+++ react/screens/DetailScreen.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Dimensions, View, StyleSheet, Text, Image, BackHandler } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { Animated, Dimensions, View, StyleSheet, Text, Image, BackHandler, TouchableOpacity } from 'react-native';
 import TouchableScale from 'react-native-touchable-scale';
 
 import { SharedElement } from 'react-navigation-shared-element';
+import Icon from 'react-native-vector-icons/Feather';
 
 const DetailScreen = props => {
 const {width, height} = Dimensions.get('window');
@@ -19,10 +20,43 @@ const {data} = props.route.params
           style={{width:width, height:width}}
           />
         </SharedElement>
-
       </View>
+      <FadeInView style={{position:'absolute', top:40, left:10}}>
+        <TouchableOpacity onPress={() => props.navigation.goBack()}>
+        <Icon name="arrow-left" color="white" style={{}} size={24}/>
+        </TouchableOpacity>
+      </FadeInView>
     </View>
   );
  };
+
+ const styles = StyleSheet.create({
+
+  });
+
+  const FadeInView = (props) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver:true
+      }
+    ).start();
+  }, [])
+
+  return (
+    <Animated.View                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
 
 export default DetailScreen;
